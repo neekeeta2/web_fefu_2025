cc337e9be48298a4fc9db0e12f5af01a361d2b98
Nikita Mishkin <mishkin.nse@dvfu.ru>
2025-10-13 21:03:22 +1000

    created base structure of my django project

---
+++ webtech_lab1/fefu_lab/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse, Http404
+from django.shortcuts import render
+
+def home_page(request):
+    return HttpResponse("Добро пожаловать на главную страницу!")
+
+def about_page(request):
+    return HttpResponse("Страница о нас")
+
+def student_profile(request, student_id):
+    if student_id > 100:
+        raise Http404("Студент не найден")
+    return HttpResponse(f"Профиль студента с ID: {student_id}")
+
+def course_detail(request, course_slug):
+    return HttpResponse(f"Информация о курсе: {course_slug}")
