717a485791115f15f37f3d86127493222a61f82f
neekeeta2 <nikitap2345@gmail.com>
2026-02-03 15:50:05 +1000

    laba 5

--- web_2025/web_2025/settings.py
+++ web_2025/web_2025/settings.py
@@ -75,10 +75,22 @@ WSGI_APPLICATION = 'web_2025.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'fefu_lab_db',
+        'USER': 'fefu_user',
+        'PASSWORD': 'asddsadsa',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
